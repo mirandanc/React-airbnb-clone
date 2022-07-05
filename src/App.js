3b80e4dd294c72617fc79fa593b1d68666a7fba3
mirandanc <miranda_chaves@hotmail.com>
2022-07-05 19:22:22 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,32 +1,78 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import Flat from './components/flat';
+import Marker from './components/marker';
 import './App.css';
-import Card from './components/card';
 
-function App() {
-  const card =   {
-    "id": 205,
-    "name": "Splendide terrasse vue imprenable",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg",
-    "price": 115,
-    "priceCurrency": "EUR",
-    "lat": 48.881840,
-    "lng": 2.343371
-  };
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: [],
+      allFlats: [],
+      selectedFlat: null,
+      search: ""
+    };
+  }
 
-  return (
-    <div className="App">
-      <div className='main'>
-        <div className='search'>
+  componentDidMount() {
+    fetch("https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          allFlats: data,
+          flats: data,
+          selectedFlat: data[0]
+        })
+      })
+  }
+
+  selectFlat = (flat) => {
+    this.setState({
+      selectedFlat: flat
+    });
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    })
+  }
+
+  render() {
+    let center = {
+      lat: 48.8566, lng: 2.3522
+    };
+    if (this.state.selectedFlat) {
+      center = { lat: this.state.selectedFlat.lat, lng: this.state.selectedFlat.lng }
+    }
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+            <input type="text" placeholder="Search..." value={this.state.search}
+              onChange={this.handleSearch} />
+          </div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat key={flat.name} flat={flat} selectFlat={this.selectFlat} />
+            })}
+          </div>
         </div>
-        <div className='cards'>
-          <Card card={card} />
-          <Card card={card} />
-          <Card card={card} />
+        <div className="map">
+          <GoogleMapReact
+            center={center}
+            zoom={14}>
+            {this.state.flats.map((flat) => {
+              return <Marker {...flat} selected={flat === this.state.selectedFlat} />
+            })}
+          </GoogleMapReact>
         </div>
       </div>
-      <div className='map'>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
